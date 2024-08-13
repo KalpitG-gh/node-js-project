e6ea41e732cd0f3ae20bd742dde046da27c22258
Kalpit Gudigar <153449924+KalpitG-gh@users.noreply.github.com>
2024-08-13 14:00:12 +0530

    Update Welcome.jsx

--- chat-app-react-nodejs-master/public/src/components/Welcome.jsx
+++ chat-app-react-nodejs-master/public/src/components/Welcome.jsx
@@ -1,22 +1,34 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Robot from "../assets/robot.gif";
+
 export default function Welcome() {
   const [userName, setUserName] = useState("");
-  useEffect(async () => {
-    setUserName(
-      await JSON.parse(
-        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-      ).username
-    );
+
+  useEffect(() => {
+    async function fetchUserName() {
+      try {
+        const storedUser = JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        );
+        if (storedUser && storedUser.username) {
+          setUserName(storedUser.username);
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    }
+    
+    fetchUserName();
   }, []);
+
   return (
     <Container>
       <img src={Robot} alt="" />
       <h1>
         Welcome, <span>{userName}!</span>
       </h1>
-      <h3>Please select a chat to Start messaging.</h3>
+      <h3>Please select a chat to start messaging.</h3>
     </Container>
   );
 }
